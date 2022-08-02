bd77df208b5dd142f23c42fb5e89c3fdc78b2bf7
Rita Uzoma <you@example.com>
2022-08-02 09:44:09 +0100

    fixed useEffect dependency error by wrapping the array function in the useEffect

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import MyModal from './Modal'
+import React, { useState, useEffect } from "react";
+import MyModal from "./Modal";
 
 export default function Page() {
-  const [data, setData] = useState([], 'Page')
-  const [loading, setLoading] = useState(false)
-  const [search, setSearch] = useState('')
-  const [filtered, setFiltered] = useState([])
-  const [openModal, setOpenModal] = useState(false)
-  const [singleMonster, setSingleMonster] = useState(false)
+  const [data, setData] = useState([], "Page");
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [singleMonster, setSingleMonster] = useState(false);
 
   // set the search state
   // const [filter, setFilter] = useState([])
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   function getData() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch((err) => console.log('error'))
-      .finally(() => setLoading(false))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log("error"))
+      .finally(() => setLoading(false));
   }
 
   // Get the event
   function handleChange(event) {
-    setSearch(event.target.value)
+    setSearch(event.target.value);
   }
 
-  // map through the array and check for any item.name that includes search
-  function filteredArray() {
-    let filtered = data.filter((item) => {
-      return item.name.toLowerCase().includes(search.toLowerCase())
-    })
-    // let filtered = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()) )
-    setFiltered(filtered)
-  }
+  // // map through the array and check for any item.name that includes search
+  // const filteredArray = useCallback(() => {
+  //   let filtered = data.filter((item) => {
+  //     return item.name.toLowerCase().includes(search.toLowerCase())
+  //   })
+  //   // let filtered = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()) )
+  //   setFiltered(filtered)
+  // },[data, search])
 
-  
   // useCallback(
   //   () => {
   //     callback
@@ -45,11 +44,19 @@ export default function Page() {
   //   [input],
   // )
   useEffect(() => {
-    filteredArray()
-  },[search, data])
+    // map through the array and check for any item.name that includes search
+    const filteredArray = () => {
+      let filtered = data.filter((item) => {
+        return item.name.toLowerCase().includes(search.toLowerCase());
+      });
+      // let filtered = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()) )
+      setFiltered(filtered);
+    };
+    filteredArray();
+  }, [search, data]);
 
   if (loading) {
-    return 'loading'
+    return "loading";
   }
 
   console.log(data);
@@ -57,30 +64,43 @@ export default function Page() {
     <>
       <div>
         <h1>Monster Rolodex</h1>
-        <input className='search' type='search' onChange={handleChange} value={search} placeholder='Search.....' />
-        <div className='container'>
-          {filtered.map(item => {
+        <input
+          className="search"
+          type="search"
+          onChange={handleChange}
+          value={search}
+          placeholder="Search....."
+        />
+        <div className="container">
+          {filtered.map((item) => {
             return (
-              <div className='cards' key={item.id}
+              <div
+                className="cards"
+                key={item.id}
                 onClick={() => {
-                  setOpenModal(true)
-                  setSingleMonster(item)
+                  setOpenModal(true);
+                  setSingleMonster(item);
                 }}
               >
-                <img src={`https://robohash.org/${item.id}?set=set2&size=180x180`} alt='' />
+                <img
+                  src={`https://robohash.org/${item.id}?set=set2&size=180x180`}
+                  alt=""
+                />
                 <h2>{item.name}</h2>
                 <p>{item.email}</p>
               </div>
-            )
+            );
           })}
         </div>
-        {filtered.length === 0 && <div className='errorMes'>Does not match any Monster</div>}
+        {filtered.length === 0 && (
+          <div className="errorMes">Does not match any Monster</div>
+        )}
       </div>
       <div className="modal">
-       {data.length > 0 &&  <MyModal open={openModal} close={setOpenModal} rita={singleMonster} />}
+        {data.length > 0 && (
+          <MyModal open={openModal} close={setOpenModal} rita={singleMonster} />
+        )}
       </div>
-
     </>
-     
-  )  
+  );
 }
